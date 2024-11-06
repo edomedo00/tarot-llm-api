d5e7d6b685f2de787a021f4976e197d8d3c5875f
edomedo <edramede@gmail.com>
2024-11-05 20:28:53 -0600

    model switched

--- src/models/llm-setup.ts
+++ src/models/llm-setup.ts
@@ -9,8 +9,8 @@ const modelsFolderDirectory = path.join(__dirname, "../..", "models");
 const llama = await getLlama();
 
 const model = await llama.loadModel({
-    // modelPath: path.join(modelsFolderDirectory, "Meta-Llama-3.1-8B-Instruct.Q4_K_M.gguf")
-    modelPath: path.join(modelsFolderDirectory, "unsloth.Q4_K_M.gguf")
+    modelPath: path.join(modelsFolderDirectory, "Meta-Llama-3.1-8B-Instruct.Q4_K_M.gguf")
+    // modelPath: path.join(modelsFolderDirectory, "unsloth.Q4_K_M.gguf")
 });
 console.log(chalk.yellow("Model loaded."));
 
